dd23cdd274a9375c467b588cbda6fbe7a09d5ffd
Richard Felintro <Richard Felintro>
2024-04-10 17:26:18 -0300

    Funcionalidade de e-mail adicionada.

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c28deca57b9aa54927fe443843020ddaac2efac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4559cb1304899dff2ad1d92af8dcec24213a363b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
