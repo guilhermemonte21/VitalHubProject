bb0baed46d8d1a7df80f58c5068fc925c5ca7186
Guilherme Pereira Monte <6352573355@info.local>
2024-05-09 15:26:24 -0300

    bug do IOS resolvido

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb2a1e4a8200e0c24c2056dfe38bc895b80b9cdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096a73a186bd23c22efcd8d232ad1673fa9c6be1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
